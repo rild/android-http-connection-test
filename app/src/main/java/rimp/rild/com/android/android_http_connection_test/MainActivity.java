37a8818ef85f0b0d8f62eacbeac919504f74947c
Iida Taiki <rild3178ata@gmail.com>
2016-07-31 10:07:35 +0900

    apply retro lambda

--- app/src/main/java/rimp/rild/com/android/android_http_connection_test/MainActivity.java
+++ app/src/main/java/rimp/rild/com/android/android_http_connection_test/MainActivity.java
@@ -4,7 +4,9 @@ import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -12,7 +14,10 @@ public class MainActivity extends AppCompatActivity {
     private final static int WC = LinearLayout.LayoutParams.WRAP_CONTENT;
     private final static int MP = LinearLayout.LayoutParams.MATCH_PARENT;
     private final static String TAG_READ = "read";
-    private EditText editText;
+
+    LinearLayout layout;
+    EditText editText;
+
     private String text;
     //android.osのものであることに注意
     private Handler handler = new Handler();
@@ -30,8 +35,37 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         //create layout
+        createLayout();
+    }
+
+    private void createLayout() {
         LinearLayout layout = new LinearLayout(this);
         layout.setBackgroundColor(Color.WHITE);
         layout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(layout);
+    }
+
+    private void createEditText() {
+        editText = new EditText(this);
+        editText.setText("");
+        editText.setLayoutParams(new LinearLayout.LayoutParams(MP, WC));
+        layout.addView(editText);
+    }
+
+    //したの makeButton メソッドを作ってからつくるように
+    private void createButton() {
+
+    }
+
+    private void makeButton(String text, String tag) {
+        Button button = new Button(this);
+        button.setText(text);
+        button.setTag(tag);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
